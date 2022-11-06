a98e47288ee5f95b54bccb9c93e13cc831c832fd
harpua555 <93162369+harpua555@users.noreply.github.com>
2022-11-06 10:58:16 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,8 @@ else
 	cp -n ${SLINGER_APP}/config.ini ${SLINGER_CONF}/config.ini
 	cp -n ${SLINGER_APP}/remote.txt ${SLINGER_CONF}/sample_remote.txt
 	cp -n ${SLINGER_APP}/remote.txt ${SLINGER_CONF}/sample_remote2.txt
-	#cp  ${SLINGER_APP}/CustomRemotes ${SLINGER_CONF}/CustomRemotes
-	#cp  ${SLINGER_APP}/Documentation ${SLINGER_CONF}/Documentation
+	cp  ${SLINGER_APP}/CustomRemotes ${SLINGER_CONF}/CustomRemotes
+	cp  ${SLINGER_APP}/Documentation ${SLINGER_CONF}/Documentation
 	
 	python3 -u ${SLINGER_APP}/slingbox_server.py ${SLINGER_CONF}/config.ini
 fi
